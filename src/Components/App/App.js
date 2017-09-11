602b971fa881b85a87469353453d1613139cd940
raroa <darkkcity@yahoo.com>
2017-09-11 17:52:09 +0300

    Codecademy generated from the project submission page.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import { Spotify } from './util/Spotify'
 
 class App extends React.Component {
     constructor(props) {
@@ -40,10 +41,15 @@ class App extends React.Component {
 
     savePlaylist() {
         let trackURIs = this.playlistTracks.map(uriValue => uriValue.uri);
+        Spotify.savePlaylist();
+        this.setState({
+            playlistName: 'New Playlist',
+            searchResults: []
+        });
     }
 
     search(term) {
-        console.log(term);
+        Spotify.search(term);
     }
 
     render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,7 @@
 const clientID = '8bcaf185467648fb8f7ec1ca4d479f5f';
 const redirectURI= 'http://localhost:3000/';
 const accessToken = '';
-let Sportify = {
+export let Spotify = {
     getAccessToken() {
         if(accessToken != null) {
             return accessToken;
@@ -12,6 +12,46 @@ let Sportify = {
          } else if(accessToken == null && ) { // did not know what had to write after &&
              window.location(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`)
          }
+    },
+
+    search(searchTerm) {
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=searchTerm`, {
+            headers: {Authorisation: `Bearer ${accessToken}`}
+        }).then(response => {return response.json()}).then(jsonResponse => {
+            if(jsonResponse.tracks) {
+                return jsonResponse.tracks.map(
+                    track => (
+                        {
+                          ID: track.id,
+                          Name: track.name,
+                          Artist: track.artists[0].name,
+                          Album: track.album.name,
+                          URI: track.uri
+                        }
+                    )
+                )
+            }
+        })
+    },
+
+    savePlaylist(playlistName, trackURIs) {
+        if(playlistName == null || trackURIs == null ) {
+            return
+        } else {
+            const TokenAccess = '2b8d67d7f9af4daca30c5f76b58da968';
+            const headers =  {headers: {
+                'Authorization': 'Bearer ' + TokenAccess
+            }};
+            const userID = '';
+
+            return fetch(`https://api.spotify.com/v1/me`, {headers: headers}).then(
+                response => {return response.json}
+            ).fetch(
+                `https://api.spotify.com/v1/users/${userID}/playlists`, {headers: headers, method: 'POST', body: JSON.stringify({id: '200'})}
+            ).then(response => {
+                let playlistID = response.json();
+            })
+        }
     }
 }
 
